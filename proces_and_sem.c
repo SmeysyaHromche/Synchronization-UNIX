b87af1d175c61f7625cee5ee5e6a067bfd76659d
SmeysyaHromche <94641011+SmeysyaHromche@users.noreply.github.com>
2023-04-27 23:23:11 +0200

    Update proces_and_sem.c

--- proces_and_sem.c
+++ proces_and_sem.c
@@ -1,67 +1,31 @@
-#include "kernel.h"
-
-const char act_text[9][30] = {"started", "going home", "entering office for service", "called by office worker", "serving a service of type", "service finished", "taking break", "break finished", "closing"};
-
-
-void file_creating(){  // definition func for open writting stream in output file
-    file = fopen("proj2.out", "w");  // open stream in file
-    if(file == NULL){
-        fprintf(stderr, "Error! Error in openning file 'proj2.out' for output.\n");
-        exit(1);
-    }
-}
-
-void write_output(Shared_memory_t *post, int service, bool customer, bool clerk, int id, int act){
-    sem_wait(&post->output_sem);
-    fprintf(file, "%d: ", post->line);
-    if(customer){
-        fprintf(file, "Z ");
-    }
-    if(clerk){
-        fprintf(file, "U ");
-    }
-    if(!customer && !clerk){
-        fprintf(file, "%s\n", act_text[act]);
-        post->line++;
-        sem_post(&post->output_sem);
-        return;
-    }
-    fprintf(file, "%d: %s  ", id, act_text[act]);
-    if(act == 2 || act == 4){
-        fprintf(file, "%d", service);
-    }
-    fprintf(file, "\n");
-    post->line++;
-    sem_post(&post->output_sem);
-}
-
+#include "proj2.h"
 
 void clerk_proces(Shared_memory_t *post, int id){
     int choose_service;
     write_output(post, 0, false, true, id, 0);
-    while(post->post_live || post->serves_1 > 0 || post->serves_2 > 0 || post->serves_3 > 0){
-        if(post->serves_1 > 0){
+    while(post->post_live || post->service_1 > 0 || post->service_2 > 0 || post->service_3 > 0){
+        if(post->service_1 > 0){
             choose_service = 1;
-            sem_wait(&post->serves1_sem);
-            post->serves_1--;
-            sem_post(&post->serves1_sem);
-            sem_post(&post->serves1_queue_sem);
+            sem_wait(&post->service1_sem);
+            post->service_1--;
+            sem_post(&post->service1_sem);
+            sem_post(&post->service1_queue_sem);
             write_output(post, choose_service, false, true, id, 4);
         }
-        else if(post->serves_2 > 0){
+        else if(post->service_2 > 0){
             choose_service = 2;
-            sem_wait(&post->serves2_sem);
-            post->serves_2--;
-            sem_post(&post->serves2_sem);
-            sem_post(&post->serves2_queue_sem);
+            sem_wait(&post->service2_sem);
+            post->service_2--;
+            sem_post(&post->service2_sem);
+            sem_post(&post->service2_queue_sem);
             write_output(post, choose_service, false, true, id, 4);
         }
-        else if(post->serves_3 > 0){
+        else if(post->service_3 > 0){
             choose_service = 3;
-            sem_wait(&post->serves3_sem);
-            post->serves_3--;
-            sem_post(&post->serves3_sem);
-            sem_post(&post->serves3_queue_sem);
+            sem_wait(&post->service3_sem);
+            post->service_3--;
+            sem_post(&post->service3_sem);
+            sem_post(&post->service3_queue_sem);
             write_output(post, choose_service, false, true, id, 4);
         }
         else{
@@ -90,19 +54,19 @@ void customer_proces(Shared_memory_t *post, int id){
     int choose_service = rand() % 3 + 1;  // client choose servec(or 1 or 2 or 3)
     switch (choose_service){
         case 1:
-            sem_wait(&post->serves1_sem);
-            post->serves_1++;
-            sem_post(&post->serves1_sem);
+            sem_wait(&post->service1_sem);
+            post->service_1++;
+            sem_post(&post->service1_sem);
             break;
         case 2:
-            sem_wait(&post->serves2_sem);
-            post->serves_2++;
-            sem_post(&post->serves2_sem);
+            sem_wait(&post->service2_sem);
+            post->service_2++;
+            sem_post(&post->service2_sem);
             break;
         case 3:
-            sem_wait(&post->serves3_sem);
-            post->serves_3++;
-            sem_post(&post->serves3_sem);
+            sem_wait(&post->service3_sem);
+            post->service_3++;
+            sem_post(&post->service3_sem);
             break;
         default:
             break;
@@ -110,13 +74,13 @@ void customer_proces(Shared_memory_t *post, int id){
     write_output(post, choose_service, true, false, id, 2);
     switch (choose_service){
         case 1:
-            sem_wait(&post->serves1_queue_sem);
+            sem_wait(&post->service1_queue_sem);
             break;
         case 2:
-            sem_wait(&post->serves2_queue_sem);
+            sem_wait(&post->service2_queue_sem);
             break;
         case 3:
-            sem_wait(&post->serves3_queue_sem);
+            sem_wait(&post->service3_queue_sem);
             break;
         default:
             break;
@@ -128,15 +92,15 @@ void customer_proces(Shared_memory_t *post, int id){
 }
 
 int semaphors_init(Shared_memory_t *post){
-    if(sem_init(&post->serves1_sem, 1, 1) == -1){
+    if(sem_init(&post->service1_sem, 1, 1) == -1){
         fprintf(stderr, "Error! Failed inittialization of semaphor.\n");
         return 1;
     }
-    if(sem_init(&post->serves2_sem, 1, 1) == -1 ){
+    if(sem_init(&post->service2_sem, 1, 1) == -1 ){
         fprintf(stderr, "Error! Failed inittialization of semaphor.\n");
         return 1;
     }
-    if(sem_init(&post->serves3_sem, 1, 1) == -1 ){
+    if(sem_init(&post->service3_sem, 1, 1) == -1 ){
         fprintf(stderr, "Error! Failed inittialization of semaphor.\n");
         return 1;
     }
@@ -144,30 +108,30 @@ int semaphors_init(Shared_memory_t *post){
         fprintf(stderr, "Error! Failed inittialization of semaphor.\n");
         return 1;
     }
-    if(sem_init(&post->serves1_queue_sem, 1, 0) == -1){
+    if(sem_init(&post->service1_queue_sem, 1, 0) == -1){
         fprintf(stderr, "Error! Failed inittialization of semaphor.\n");
         return 1;
     }
-    if(sem_init(&post->serves2_queue_sem, 1, 0) == -1 ){
+    if(sem_init(&post->service2_queue_sem, 1, 0) == -1 ){
         fprintf(stderr, "Error! Failed inittialization of semaphor.\n");
         return 1;
     }
-    if(sem_init(&post->serves3_queue_sem, 1, 0) == -1 ){
+    if(sem_init(&post->service3_queue_sem, 1, 0) == -1 ){
         fprintf(stderr, "Error! Failed inittialization of semaphor.\n");
         return 1;
     }
     return 0;
 }
 int semaphors_destroy(Shared_memory_t *post){
-    if(sem_destroy(&post->serves1_sem) == -1 ){
+    if(sem_destroy(&post->service1_sem) == -1 ){
         fprintf(stderr, "Error! Failed destruction of semaphor.\n");
         return 1;
     }
-    if(sem_destroy(&post->serves2_sem) == -1 ){
+    if(sem_destroy(&post->service2_sem) == -1 ){
         fprintf(stderr, "Error! Failed destruction of semaphor.\n");
         return 1;
     }
-    if(sem_destroy(&post->serves3_sem) == -1){
+    if(sem_destroy(&post->service3_sem) == -1){
         fprintf(stderr, "Error! Failed destruction of semaphor.\n");
         return 1;
     } 
@@ -175,15 +139,15 @@ int semaphors_destroy(Shared_memory_t *post){
         fprintf(stderr, "Error! Failed destruction of semaphor.\n");
         return 1;
     }
-    if(sem_destroy(&post->serves1_queue_sem)  == -1 ){
+    if(sem_destroy(&post->service1_queue_sem)  == -1 ){
         fprintf(stderr, "Error! Failed destruction of semaphor.\n");
         return 1;
     }
-    if(sem_destroy(&post->serves2_queue_sem)  == -1 ){
+    if(sem_destroy(&post->service2_queue_sem)  == -1 ){
         fprintf(stderr, "Error! Failed destruction of semaphor.\n");
         return 1;
     }
-    if(sem_destroy(&post->serves3_queue_sem)  == -1 ){
+    if(sem_destroy(&post->service3_queue_sem)  == -1 ){
         fprintf(stderr, "Error! Failed destruction of semaphor.\n");
         return 1;
     }
