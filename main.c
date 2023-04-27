62affe0be544853e531b1ec7e766d9cd26d50e5b
SmeysyaHromche <94641011+SmeysyaHromche@users.noreply.github.com>
2023-04-27 23:22:44 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,57 +1,62 @@
-#include "kernel.h"
+#include "proj2.h"
 
 int main(int argc, char *argv[]){
+    
     int arg_value[SIZE_ARG_LINE];  // array with int value of arg line
     analys_arg_line(argc, argv, arg_value);  // parsing and analysing of command line
-    Shared_memory_t *post = NULL;
-    post = sh_m_create();
-    sh_m_new_value(arg_value, post);
-    if(semaphors_init(post)){
+    
+    Shared_memory_t *post = NULL;  // abstraction "Post" (shared memory)
+    post = sh_m_create();  // allocate shared memory
+    sh_m_new_value(arg_value, post);  // specification shared memory
+    if(semaphors_init(post)){  // init semaphors in shared memory
         sh_m_clean(post);
         exit (1);
     }
+    
     file_creating();  // create file for output
-    setbuf(file, 0);  // cancel buffering for output file (writting maximum speed)
-    pid_t id_customer[post->num_customer];
-    pid_t id_clerk[post->num_clerk];
+    
+    pid_t id_customer[post->num_customer];  // array for identification customers process with pid
+    pid_t id_clerk[post->num_clerk];  // array for identification clerks proces with pid
 
-    for(int i = 0; i < post->num_clerk; i++){
-        id_clerk[i] = fork();
-        if(id_clerk[i] == 0){
+    for(int i = 0; i < post->num_clerk; i++){  // start customers process
+        id_clerk[i] = fork();  // making new process
+        if(id_clerk[i] == 0){  // child proces
             clerk_proces(post,(i+1));
         }
-        else if(id_clerk[i] == 0){
-            fprintf(stderr, "Error! Fail of clerks fork\n");
+        else if(id_clerk[i] == 0){  // error state after forking
+            fprintf(stderr, "Error! Fail of clerks fork.\n");
             fclose(file);  // closing output
-            semaphors_destroy(post);
-            sh_m_clean(post);
+            semaphors_destroy(post);  // clean semaphors
+            sh_m_clean(post);  // clean shared memory
             exit(1);
         }
     }
-    for(int i = 0; i < post->num_customer; i++){
-        id_customer[i] = fork();
-        if(id_customer[i] == 0){
+    
+    for(int i = 0; i < post->num_customer; i++){  // start clients proces
+        id_customer[i] = fork();  // making new process
+        if(id_customer[i] == 0){  // child proces
             customer_proces(post,(i+1));
         }
-        else if(id_customer[i] == 0){
+        else if(id_customer[i] == 0){  // error state after forking
             fprintf(stderr, "Error! Fail of clerks fork\n");
             fclose(file);  // closing output
-            semaphors_destroy(post);
-            sh_m_clean(post);
+            semaphors_destroy(post);  // clean semaphors
+            sh_m_clean(post);  // clean shared memory
             exit(1);
         }
     }
-    time_to_sleep(post->max_time_close_post, true);
-    post->post_live = false;
+    
+    time_to_sleep(post->max_time_close_post, true);  // sleeping befor closing posts
+    post->post_live = false;  // close post
     write_output(post, 0, false, false, 0, 8);
     //while(wait(NULL)>0);
-    if(semaphors_destroy(post)){
+    
+    if(semaphors_destroy(post)){  // error state if failed sem_destroy()
         fclose(file);  // closing output
-        sh_m_clean(post);
+        sh_m_clean(post);  // clean shared memory
         exit(1);
     }
     fclose(file);  // closing output
-    sh_m_clean(post);
+    sh_m_clean(post);  // clean shared memory
     exit(0);
-    return 0;
 }
