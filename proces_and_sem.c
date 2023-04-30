f18823e5cbddb8436e245b6c082eeea562f7d482
SmeysyaHromche <94641011+SmeysyaHromche@users.noreply.github.com>
2023-04-30 19:56:52 +0200

    Update proces_and_sem.c

--- proces_and_sem.c
+++ proces_and_sem.c
@@ -31,7 +31,13 @@ void clerk_proces(Shared_memory_t *post, int id){
             write_output(post, choose_service, false, true, id, 4);  // serving
         }
         else{  // if in post dont be customer
+            sem_wait(&post->check_clos_sem);  // wait mutex for sinchronizate with closing post for going customers and breaaking work clerks
+            if(!post->post_live){
+                sem_post(&post->check_clos_sem);  // post mutex for sinchronizate with closing post for going customers and breaaking work clerks
+                break;
+            }
             write_output(post, 0, false, true, id, 6);  // going in break
+            sem_post(&post->check_clos_sem);  // post mutex for sinchronizate with closing post for going customers and breaaking work clerks
             time_to_sleep(post->max_time_break, false);  // sleep
             write_output(post, 0, false, true, id, 7);  // back form break
             continue;
@@ -39,7 +45,9 @@ void clerk_proces(Shared_memory_t *post, int id){
         time_to_sleep(10, false);  // sleep
         write_output(post, choose_service, false, true, id, 5);  // end serving   
     }
+    sem_wait(&post->clos_sem);  // wait sem for sinchronizate with going clerk home
     write_output(post, 0, false, true, id, 1);  // go to home
+    sem_post(&post->clos_sem);  // post sem for sinchronizate with going clerk home
     exit(0);
 }
 
@@ -49,33 +57,34 @@ void customer_proces(Shared_memory_t *post, int id){
     srand(getpid()*time(NULL));  // start generic random value based on time and random pid
     write_output(post, 0, true, false, id, 0);  // start
     time_to_sleep(post->max_time_wait, false);  // sleep
+    sem_wait(&post->check_clos_sem);  // wait mutex for sinchronizate with closing post for going customers and breaaking work clerks
     if(!post->post_live){  // check if post close
         write_output(post, 0, true, false, id, 1);  // go to home
+        sem_post(&post->check_clos_sem);  // post mutex for sinchronizate with closing post for going customers and breaaking work clerks
         exit(0);
     }
     int choose_service = rand() % 3 + 1;  // client choose servec
     switch (choose_service){
         case 1:  // if service 1
             sem_wait(&post->service1_sem);  // wait mutex for count in queue
-            write_output(post, choose_service, true, false, id, 2);  // waiting clerk
             post->service_1++;  // inc count in queue
             sem_post(&post->service1_sem);  // post mutex for count in queue
             break;
         case 2:
             sem_wait(&post->service2_sem);  // wait mutex for count in queue
-            write_output(post, choose_service, true, false, id, 2);  // waiting clerk
             post->service_2++;  // inc count in queue
             sem_post(&post->service2_sem);  // post mutex for count in queue
             break;
         case 3:
             sem_wait(&post->service3_sem);  // wait mutex for count in queue
-            write_output(post, choose_service, true, false, id, 2);  // waiting clerk
             post->service_3++;  // inc count in queue
             sem_post(&post->service3_sem);  // post mutex for count in queue
             break;
         default:
             break;
     } 
+    write_output(post, choose_service, true, false, id, 2);  // waiting clerk
+    sem_post(&post->check_clos_sem);  // post mutex for sinchronizate with closing post for going customers and breaaking work clerks
     switch (choose_service){
         case 1:
             sem_wait(&post->service1_queue_sem);  // invited by a clerk
@@ -125,6 +134,14 @@ int semaphors_init(Shared_memory_t *post){
         fprintf(stderr, "Error! Failed inittialization of semaphor.\n");
         return 1;
     }
+    if(sem_init(&post->clos_sem, 1, 0) == -1 ){  // init semaphor for sinchronizate with going clerk home and check error
+        fprintf(stderr, "Error! Failed inittialization of semaphor.\n");
+        return 1;
+    }
+    if(sem_init(&post->check_clos_sem, 1, 1) == -1 ){  // init mutex for sinchronizate with closing post for going customers and breaaking work clerks and check error
+        fprintf(stderr, "Error! Failed inittialization of semaphor.\n");
+        return 1;
+    }
     return 0;
 }
 
@@ -155,6 +172,14 @@ int semaphors_destroy(Shared_memory_t *post){
         return 1;
     }
     if(sem_destroy(&post->service3_queue_sem)  == -1 ){  // destroy semaphor for 3 servis queue and check error
+        fprintf(stderr, "Error! Failed destruction of semaphor.\n");
+        return 1;
+    }
+    if(sem_destroy(&post->clos_sem)  == -1 ){  // destroy semaphor for sinchronizate with going clerk home and check error
+        fprintf(stderr, "Error! Failed destruction of semaphor.\n");
+        return 1;
+    }
+    if(sem_destroy(&post->check_clos_sem)  == -1 ){  // destroy mutex for sinchronizate with closing post for going customers and breaaking work clerks and check error
         fprintf(stderr, "Error! Failed destruction of semaphor.\n");
         return 1;
     }
